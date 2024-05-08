68390e065757c5d94ba329677b61657f561e20a7
@RODRIGUEZMLDO <j.antonio-rm@outlook.com>
2024-05-08 15:53:49 -0600

    Update Profile Files

--- SaveData/profiles.java
+++ SaveData/profiles.java
@@ -7,46 +7,17 @@ import java.io.*;
 public class profiles {
  
     String name;
-    int victories, gamePlayed , defeats;
+    int victories, gamePlayed , defeats, destroyedObjects;
 
-    public profiles(String name,int victories, int gamePlayed, int defeats){
+    public profiles(String name,int victories, int gamePlayed, int defeats, int destroyedObjects){
         this.name = name;
         this.victories = victories;
         this.gamePlayed = gamePlayed;
         this.defeats = defeats;
+        this.destroyedObjects = destroyedObjects;
     }
 
-    public static void procesoRegistro(){
-        String root = "SaveData\\DataFiles\\profiles.txt"; 
-        crearPerfiles(root);
-        Scanner inData = new Scanner(System.in);
-
-        String newUsuario = "NO", name;
-        int victories, gamePlayed, defeats;
-        
-
-        do {
-            // Solicitar datos al usuario
-            System.out.print("Ingresa el nombre: ");
-            name = inData.nextLine();
-            System.out.print("Ingresa el número de victorias: ");
-            victories = Integer.parseInt(inData.nextLine());
-            System.out.print("Ingresa el número de partidas: ");
-            gamePlayed = Integer.parseInt(inData.nextLine());
-            System.out.print("Ingresa el número de derrotas: ");
-            defeats = Integer.parseInt(inData.nextLine());
-
-            registroPerfiles(root, name, victories, gamePlayed, defeats);
-
-            System.out.print("Nuevo usuario: ");
-            newUsuario = inData.nextLine();
-
-        } while (newUsuario.equals("Si"));
-
-        inData.close();
-    }
-
-    public static void crearPerfiles(String nameFile){
+    public static void creatingProfileFile(String nameFile){
         File file = new File(nameFile);
 
         if (!file.exists()) {
@@ -57,21 +28,34 @@ public class profiles {
                 ex.printStackTrace(System.out);
             }
         } else {
-            System.out.println("El archivo ya existe.");
+            
         }
     }
 
-    public static void registroPerfiles(String nameFile, String name, int victories, int gamePlayed, int defeats) {
-    
-        try {
-            // Escribir los datos en el archivo
-            BufferedWriter writer = new BufferedWriter(new FileWriter(nameFile, true)); // true para agregar al final del archivo
-            writer.write(name + "," + victories + "," + gamePlayed + "," + defeats);
-            writer.newLine(); // Nueva línea para el próximo registro
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void registeringProfiles(String nameFile, String name, int victories, int gamePlayed, int defeats, int destroyedObjects) {
+        String root = "SaveData\\DataFiles\\profiles.txt"; 
+        creatingProfileFile(root);
+        Scanner inData = new Scanner(System.in);
+
+        String newUsuario = "NO";
+
+        do {
+            try {
+                // Escribir los datos en el archivo
+                BufferedWriter writer = new BufferedWriter(new FileWriter(nameFile, true)); // true para agregar al final del archivo
+                writer.write(name + "," + victories + "," + gamePlayed + "," + defeats + "," + destroyedObjects);
+                writer.newLine(); // Nueva línea para el próximo registro
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.print("Nuevo usuario: ");
+            newUsuario = inData.nextLine();
+
+        } while (newUsuario.equals("Si"));
+
+        inData.close();
     }
     
 }
