410926796e2b965168be18b270773831fef7d83a
Naidu3736 <franciscoapa9693736@gmail.com>
2024-05-09 00:45:30 -0600

    assets update

--- Graphics/Assets.java
+++ Graphics/Assets.java
@@ -15,7 +15,7 @@ public class Assets {
 	// effects
 	
 	public static BufferedImage[] shipSpeed = new BufferedImage[2]; 
-	public static BufferedImage shipCadence;
+	public static BufferedImage[] shipCadence = new BufferedImage[2];
 	public static BufferedImage[] shipReduceSpeed = new BufferedImage[2];
 	public static BufferedImage shipHealth;
 	
@@ -50,44 +50,34 @@ public class Assets {
 	
 	public static void init()
 	{
-		shipCadence = loadImage("nave_powerup_cadenia.gif");
+		shipHealth = loadImage("/Effects/shipRegenerate.gif");
+		
+		laser = loadImage("/Effects/laser.gif");
+		
+		meteors = loadImage("/CosmicDanger/asteroid.gif");
 
-		shipHealth = loadImage("nave_recuperando_vida.gif");
-		
-		laser = loadImage("lasegreen.gif");
-		
-		meteors = loadImage("asteroide.gif");
+		blackHole = loadImage("/CosmicDanger/blackHola.gif");
 
-		blackHole = loadImage("agujero_negro.gif");
+		cadence = loadImage("/PowerUp/powerupCadence.gif");
 
-		cadence = loadImage("powerup_cadencia.gif");
+		speed = loadImage("/PowerUp/powerupBoost.gif");
 
-		speed = loadImage("powerup_boost.gif");
+		reduceSpeed = loadImage("/PowerUp/powerupReduce.gif");
 
-		reduceSpeed = loadImage("powerup_reduce.gif");
-
-		health = loadImage("vida.gif");
+		health = loadImage("/PowerUp/powerupHealth.gif");
 		
-		for(int i = 0; i < 3; i++)
-			shieldEffect[i] = loadImage("/effects/shield" + (i + 1) +".png"); 
-		
-		for(int i = 0; i < bigs.length; i++)
-			bigs[i] = loadImage("/meteors/big"+(i+1)+".png");
-		
-		for(int i = 0; i < meds.length; i++)
-			meds[i] = loadImage("/meteors/med"+(i+1)+".png");
-		
-		for(int i = 0; i < smalls.length; i++)
-			smalls[i] = loadImage("/meteors/small"+(i+1)+".png");
-		
-		for(int i = 0; i < tinies.length; i++)
-			tinies[i] = loadImage("/meteors/tiny"+(i+1)+".png");
+		for(int i = 0; i < 2; i++) {
+			player[i] = loadImage("/Player/ship" + i +".gif"); 
+			shipSpeed[i] = loadImage("/Effects/shipBoost" + i + ".gif");
+			shipCadence[i] = loadImage("/Effects/shipCadence" + i + ".gif");
+			shipReduceSpeed[i] = loadImage("/Effects/shipReduce" + i + ".gif");
+		}
 		
 		for(int i = 0; i < exp.length; i++)
-			exp[i] = loadImage("/explosion/"+i+".png");
+			exp[i] = loadImage("/explosion/"+ i +".png");
 		
 		for(int i = 0; i < numbers.length; i++)
-			numbers[i] = loadImage("/numbers/"+i+".png");
+			numbers[i] = loadImage("/numbers/"+ i +".png");
 		
 		backgroundMusic = loadSound("/sounds/backgroundMusic.wav");
 		explosion = loadSound("/sounds/explosion.wav");
